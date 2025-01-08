d5b6af12b9e53c9c77f67bedbc674d8db009685a
gigablaster <gigakek@protonmail.com>
2025-01-08 12:09:21 +0500

    Example is kind of adapted to new system

--- crates/lib/kiri-runner/examples/loop.rs
+++ crates/lib/kiri-runner/examples/loop.rs
@@ -2,16 +2,17 @@
 
 use std::{error::Error, fmt::Display, sync::Arc, thread, time::Duration};
 
+use kiri::{RenderView, RenderWorld};
 use kiri_common::GameAppConfig;
 use kiri_gfx::{RenderContext, RenderTargetPool, Renderer};
-use kiri_math::{vec3, Affine3A, Vec3};
+use kiri_math::{vec3, Affine3A, PerspectiveCamera, Vec3};
 use kiri_resources::{ResourceLoader, ResourceManager};
 use kiri_runner::{run_game, GameClient, GameError, GameTickState};
 
 #[derive(Debug)]
 struct Loop {
     resources: Arc<ResourceManager>,
-    // world: World,
+    world: RenderWorld,
 }
 
 #[derive(Debug)]
@@ -33,7 +34,39 @@ static CONFIG: GameAppConfig = GameAppConfig {
 impl GameClient<LoopError> for Loop {
     fn create(renderer: &Arc<Renderer>) -> Result<Self, GameError<LoopError>> {
         let resources = ResourceManager::new(renderer)?;
-        // let mut world = World::new();
+        let world = RenderWorld::new(&resources)?;
+        world.spawn(|context| {
+            for x in -5..5 {
+                for y in -5..5 {
+                    for z in -5..5 {
+                        context.load_model(
+                            &Affine3A::from_translation(vec3(
+                                0.75 * x as f32,
+                                0.75 * y as f32,
+                                0.75 * z as f32,
+                            )),
+                            resources.get_or_load_model("FlightHelmet/FlightHelmet.gltf"),
+                        );
+                    }
+                }
+            }
+            // world.spawn(DirectionalLight {
+            //     direction: Vec3::Z,
+            //     color: vec3(15.0, 10.0, 13.0),
+            // });
+            // world.spawn((
+            //     PerspectiveCamera {
+            //         fov: 1.0,
+            //         znear: 0.1,
+            //         zfar: 100.0,
+            //     },
+            //     Transform(Affine3A::look_at_lh(
+            //         vec3(0.0, 0.5, 2.25),
+            //         Vec3::ZERO,
+            //         Vec3::Y,
+            //     )),
+            // ));
+        });
         // for x in -5..5 {
         //     for y in -5..5 {
         //         for z in -5..5 {
@@ -70,7 +103,7 @@ impl GameClient<LoopError> for Loop {
         //     bottom: vec3(0.5, 1.0, 0.5),
         // });
         // world.insert_resource(Postprocess { expouse: 0.2 });
-        Ok(Self { resources })
+        Ok(Self { world, resources })
     }
 
     fn update(&mut self, _time: kiri_common::GameTime) -> Result<GameTickState, LoopError> {
@@ -85,8 +118,28 @@ impl GameClient<LoopError> for Loop {
         context: &RenderContext,
         pool: &RenderTargetPool,
     ) -> Result<(), kiri_gfx::Error> {
-        // render_world(&mut self.world, context, &self.resources, pool)
-        todo!()
+        self.world.render(
+            RenderView {
+                camera: PerspectiveCamera {
+                    fov: 1.0,
+                    znear: 0.1,
+                    zfar: 100.0,
+                    origin: vec3(0.0, 0.5, 2.25),
+                    forward: Vec3::X,
+                    up: Vec3::Y,
+                    aspect: context.backbuffer.desc.aspect(),
+                },
+                expouse: 2.0,
+                ambient: (
+                    vec3(1.0, 1.0, 1.5),
+                    vec3(1.0, 0.5, 0.5),
+                    vec3(0.5, 1.0, 0.5),
+                ),
+            },
+            context,
+            pool,
+        )?;
+        Ok(())
     }
 
     fn config() -> &'static kiri_common::GameAppConfig<'static> {
