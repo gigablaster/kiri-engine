dcfa7562b5438dfc0b30585c7bea5b2a585e654c
gigablaster <gigakek@protonmail.com>
2025-01-19 14:38:56 +0500

    Updated copyright

--- crates/lib/kiri-backend/src/frame.rs
+++ crates/lib/kiri-backend/src/frame.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2023-2024 gigablaster
+// Copyright (C) 2023-2025 gigablaster
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
