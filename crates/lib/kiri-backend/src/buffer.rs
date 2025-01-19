b7f0677db8650e80af980691d167921f497ce88d
gigablaster <gigakek@protonmail.com>
2025-01-19 14:42:28 +0500

    Small cleanup

--- crates/lib/kiri-backend/src/buffer.rs
+++ crates/lib/kiri-backend/src/buffer.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{hash::Hash, ptr::NonNull, sync::Arc};
+use std::{ptr::NonNull, sync::Arc};
 
 use ash::vk;
 use gpu_alloc_ash::AshMemoryDevice;
